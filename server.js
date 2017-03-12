02a437a6a45a13f0ad3838b5d073a6234fe81331
Julian Cowell <jgarth123@icloud.com>
2017-03-12 02:40:54 -0500

    build dir for production and server.js to serve static files in build.

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+
+app.use(express.static('build'))
+
+app.get('/', function (req, res) {
+  res.sendfile("index.htm;")
+})
+
+var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ip);
